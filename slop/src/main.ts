1bbec5ef0511ce354a0532c6493ff94d4f35e963
Joona Luoma <joona.luoma@hotmail.com>
2025-05-21 21:00:56 +0300

    Add movement and jumping

--- slop/src/main.ts
+++ slop/src/main.ts
@@ -4,17 +4,19 @@ import {
   Engine,
   HavokPlugin,
   HemisphericLight,
+  KeyboardEventTypes,
   MeshBuilder,
   PhysicsAggregate,
   PhysicsCharacterController,
   PhysicsShapeType,
+  Quaternion,
   Scene,
-  StandardMaterial,
   UniversalCamera,
   Vector3,
 } from "@babylonjs/core";
 import { AdvancedDynamicTexture, TextBlock } from "@babylonjs/gui";
 import HavokPhysics from "@babylonjs/havok";
+import { GridMaterial } from "@babylonjs/materials";
 
 const hp = await HavokPhysics();
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
@@ -22,7 +24,6 @@ const engine = new Engine(canvas, true);
 const hk = new HavokPlugin(false, hp);
 const scene = new Scene(engine);
 const camera = new UniversalCamera("cam", new Vector3(0, 30, -10), scene);
-camera.setTarget(new Vector3(0, 15, 0));
 camera.attachControl();
 const light = new HemisphericLight("light", new Vector3(3, 10, 0), scene);
 light.intensity = 0.5;
@@ -31,30 +32,31 @@ scene.enablePhysics(new Vector3(0, -9.81, 0), hk);
 
 const ground = MeshBuilder.CreateGround("ground", { width: 32, height: 32 }, scene);
 new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
-const groundMat = new StandardMaterial("groundMat", scene);
-groundMat.diffuseColor = Color3.Green();
+const groundMat = new GridMaterial("groundMat", scene);
+groundMat.lineColor = Color3.Teal();
+groundMat.gridRatio = 1;
 ground.material = groundMat;
+ground.material.cullBackFaces = true;
 
 const characterGravity = new Vector3(0, -9.81, 0);
+const characterInputDirection = Vector3.Zero();
+const characterOrientationQuaternion = Quaternion.Zero();
+const characterAirSpeed = 7.0;
+const characterGroundSpeed = 8.0;
+let characterWantJump = false;
+const characterJumpHeight = 3.0;
+const characterJumpStartVelocityY = Math.sqrt(2 * characterGravity.length() * characterJumpHeight);
 
-const characterCapsule = MeshBuilder.CreateCapsule(
-  "character",
-  {
-    height: 2,
-    radius: 0.5,
-  },
-  scene
-);
 const characterController = new PhysicsCharacterController(new Vector3(0, 30, 0), { capsuleHeight: 2, capsuleRadius: 0.5 }, scene);
 
 const debugGui = AdvancedDynamicTexture.CreateFullscreenUI("ui", true, scene);
-const debugGuiTextBlock1 = new TextBlock("debug1", "...");
+const debugGuiTextBlock1 = new TextBlock("debug1", "debug1");
 debugGuiTextBlock1.fontSize = 24;
 debugGuiTextBlock1.top = -300;
 debugGuiTextBlock1.left = -500;
 debugGuiTextBlock1.color = "white";
 debugGui.addControl(debugGuiTextBlock1);
-const debugGuiTextBlock2 = new TextBlock("debug2", "...");
+const debugGuiTextBlock2 = new TextBlock("debug2", "debug2");
 debugGuiTextBlock2.fontSize = 24;
 debugGuiTextBlock2.top = -270;
 debugGuiTextBlock2.left = -500;
@@ -62,36 +64,108 @@ debugGuiTextBlock2.color = "white";
 debugGui.addControl(debugGuiTextBlock2);
 
 scene.onBeforeRenderObservable.add((scene) => {
-  characterCapsule.position.copyFrom(characterController.getPosition());
+  const characterPosition = characterController.getPosition();
+  camera.position.copyFrom(characterPosition);
 });
 
-scene.onAfterRenderObservable.add((scene) => {
+scene.onAfterPhysicsObservable.add((scene) => {
   if (scene.deltaTime == undefined) return;
   const dt = scene.deltaTime / 1000.0;
   if (dt == 0) return;
 
-  const support = characterController.checkSupport(dt, Vector3.DownReadOnly);
+  const surfaceInfo = characterController.checkSupport(dt, Vector3.DownReadOnly);
   let velocity: Vector3;
-  const prevVelocity = characterController.getVelocity();
-  if (support.supportedState === CharacterSupportedState.UNSUPPORTED) {
+  const currVelocity = characterController.getVelocity();
+
+  if (surfaceInfo.supportedState === CharacterSupportedState.UNSUPPORTED) {
+    const inputDesiredVelocity = characterInputDirection
+      .scale(characterAirSpeed)
+      .applyRotationQuaternion(Quaternion.FromEulerAnglesToRef(0, camera.rotation.y, 0, characterOrientationQuaternion));
     velocity = characterController.calculateMovement(
       dt,
       Vector3.RightHandedForwardReadOnly,
       Vector3.UpReadOnly,
-      prevVelocity,
+      currVelocity,
       Vector3.ZeroReadOnly,
-      Vector3.ZeroReadOnly,
+      inputDesiredVelocity,
       Vector3.UpReadOnly
     );
-    velocity.addInPlace(Vector3.UpReadOnly.scale(-velocity.dot(Vector3.UpReadOnly)));
-    velocity.addInPlace(Vector3.UpReadOnly.scale(prevVelocity.dot(Vector3.UpReadOnly)));
-    velocity.addInPlace(characterGravity.scale(dt));
+    velocity.y += currVelocity.y - velocity.y;
+    velocity.y += characterGravity.y * dt;
   } else {
-    velocity = Vector3.ZeroReadOnly;
+    if (characterWantJump) {
+      currVelocity.y = characterJumpStartVelocityY;
+      velocity = currVelocity;
+    } else {
+      const inputDesiredVelocity = characterInputDirection
+        .scale(characterGroundSpeed)
+        .applyRotationQuaternion(Quaternion.FromEulerAnglesToRef(0, camera.rotation.y, 0, characterOrientationQuaternion));
+      velocity = characterController.calculateMovement(
+        dt,
+        Vector3.RightHandedForwardReadOnly,
+        surfaceInfo.averageSurfaceNormal,
+        currVelocity,
+        surfaceInfo.averageSurfaceVelocity,
+        inputDesiredVelocity,
+        Vector3.UpReadOnly
+      );
+      velocity.addInPlace(surfaceInfo.averageSurfaceVelocity);
+    }
   }
   characterController.setVelocity(velocity);
-  characterController.integrate(dt, support, characterGravity);
-  debugGuiTextBlock1.text = `${velocity.x.toPrecision(2)},${velocity.y.toPrecision(2)},${velocity.z.toPrecision(2)}`;
+  characterController.integrate(dt, surfaceInfo, characterGravity);
+});
+
+scene.onKeyboardObservable.add((k) => {
+  switch (k.type) {
+    case KeyboardEventTypes.KEYDOWN:
+      switch (k.event.key) {
+        case "w":
+        case "W":
+          characterInputDirection.z = 1;
+          break;
+        case "s":
+        case "S":
+          characterInputDirection.z = -1;
+          break;
+        case "a":
+        case "A":
+          characterInputDirection.x = -1;
+          break;
+        case "d":
+        case "D":
+          characterInputDirection.x = 1;
+          break;
+        case " ":
+          characterWantJump = true;
+          break;
+      }
+      break;
+    case KeyboardEventTypes.KEYUP:
+      switch (k.event.key) {
+        case "w":
+        case "W":
+        case "s":
+        case "S":
+          characterInputDirection.z = 0;
+          break;
+        case "a":
+        case "A":
+        case "d":
+        case "D":
+          characterInputDirection.x = 0;
+          break;
+        case " ":
+          characterWantJump = false;
+          break;
+      }
+  }
+});
+
+canvas.addEventListener("click", () => {
+  if (!engine.isPointerLock) {
+    engine.enterPointerlock();
+  }
 });
 
 engine.runRenderLoop(() => {
