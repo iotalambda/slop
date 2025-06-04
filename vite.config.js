6361a96010d96504ccc8ce36eff1b592325b33b5
Joona Luoma <joona.luoma@hotmail.com>
2025-06-04 23:01:20 +0300

    Fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/slop/",
+  base: "/",
   optimizeDeps: {
     exclude: ["@babylonjs/havok"],
   },
